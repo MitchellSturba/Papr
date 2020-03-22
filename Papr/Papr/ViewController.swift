52f2de1f035eee33e7e8702d5e5d66c2c5de26d5
Mitch Sturba <mitchellsturba2@gmail.com>
2020-03-22 18:08:54 -0400

    Initial Push

---
+++ Papr/Papr/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Papr
+//
+//  Created by Mitchell Sturba on 2020-03-22.
+//  Copyright © 2020 Mitchell Sturba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
--- README.md
+++
@@ -1,2 +0,0 @@
-# Papr
-Papr is the iOS mobile app that tells you which stores near you are selling toilet paper during a time of crisis
