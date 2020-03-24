5140bf3f8dc9351e9deb1ae5d62636a83dd7e95e
Mitch Sturba <mitchellsturba2@gmail.com>
2020-03-24 09:22:32 -0400

    Added Unwind Segues
    Also screenshot updates

--- Papr/Papr/ViewControllers/ViewController.swift
+++ Papr/Papr/ViewControllers/ViewController.swift
@@ -14,6 +14,11 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func unwindToFirstScreen(_ unwindSegue: UIStoryboardSegue) {
+        let sourceViewController = unwindSegue.source
+        // Use data from the view controller which initiated the unwind segue
+    }
 
 
 }
